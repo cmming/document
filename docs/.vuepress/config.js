a0151fe172af8bcff6e424edaf24253d112a9099
cmming <2942257072@qq.com>
2020-01-10 23:02:20 +0800

    fix: 🐛 将后续的axios开发文档同步
    将后续的axios开发文档同步

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,18 +19,18 @@ module.exports = {
         sidebarDepth: 3,
         lastUpdated: '上次更新',
         nav: [{
-                text: '博客',
-                link: '/blog',
-                items: [
-                    { text: '前端', link: '/frontEnd/' },
-                    { text: '后端', link: '/backEnd/' },
-                    { text: 'typescript', link: '/typescript/chapter1/' },
-                ]
-            },
-            { text: '算法', link: '/algorithm/' },
-            { text: '随笔', link: '/thought/' },
-            // { text: '项目', link: '/project/' },
-            { text: 'Todo', link: '/todo/' },
+            text: '博客',
+            link: '/blog',
+            items: [
+                { text: '前端', link: '/frontEnd/' },
+                { text: '后端', link: '/backEnd/' },
+                { text: 'typescript', link: '/typescript/chapter1/' },
+            ]
+        },
+        { text: '算法', link: '/algorithm/' },
+        { text: '随笔', link: '/thought/' },
+        // { text: '项目', link: '/project/' },
+        { text: 'Todo', link: '/todo/' },
 
         ],
         sidebar: {
@@ -188,59 +188,136 @@ function getBlogSidebar(type) {
         case 'typescript':
             return [
                 {
-                  title: '初识 TypeScript',
-                  collapsable: false,
-                  children: [
-                    ['./chapter1/', 'Introduction'],
-                    './chapter1/install',
-                    './chapter1/start'
-                  ]
+                    title: '初识 TypeScript',
+                    collapsable: false,
+                    children: [
+                        ['./chapter1/', 'Introduction'],
+                        './chapter1/install',
+                        './chapter1/start'
+                    ]
                 },
                 {
-                  title: 'TypeScript 常用语法',
-                  collapsable: false,
-                  children: [
-                    './chapter2/type',
-                    './chapter2/declare',
-                    './chapter2/interface',
-                    './chapter2/class',
-                    './chapter2/function',
-                    './chapter2/generic',
-                    './chapter2/inference',
-                    './chapter2/advance'
-                  ]
+                    title: 'TypeScript 常用语法',
+                    collapsable: false,
+                    children: [
+                        './chapter2/type',
+                        './chapter2/declare',
+                        './chapter2/interface',
+                        './chapter2/class',
+                        './chapter2/function',
+                        './chapter2/generic',
+                        './chapter2/inference',
+                        './chapter2/advance'
+                    ]
                 },
                 {
-                  title: 'ts-axios 项目初始化',
-                  collapsable: false,
-                  children: [
-                    './chapter3/require',
-                    './chapter3/init',
-                    './chapter3/base'
-                  ]
+                    title: 'ts-axios 项目初始化',
+                    collapsable: false,
+                    children: [
+                        './chapter3/require',
+                        './chapter3/init',
+                        './chapter3/base'
+                    ]
                 },
                 {
-                  title: 'ts-axios 基础功能实现',
-                  collapsable: false,
-                  children: [
-                    './chapter4/url',
-                    './chapter4/data',
-                    './chapter4/header',
-                    './chapter4/response',
-                    './chapter4/response-header',
-                    './chapter4/response-data'
-                  ]
+                    title: 'ts-axios 基础功能实现',
+                    collapsable: false,
+                    children: [
+                        './chapter4/url',
+                        './chapter4/data',
+                        './chapter4/header',
+                        './chapter4/response',
+                        './chapter4/response-header',
+                        './chapter4/response-data'
+                    ]
                 },
                 {
-                  title: 'ts-axios 异常情况处理',
-                  collapsable: false,
-                  children: [
-                    './chapter5/error',
-                    './chapter5/enhance'
-                  ]
+                    title: 'ts-axios 异常情况处理',
+                    collapsable: false,
+                    children: [
+                        './chapter5/error',
+                        './chapter5/enhance'
+                    ]
+                },
+                {
+                    'title': 'ts-axios 接口扩展',
+                    collapsable: false,
+                    children: [
+                        'chapter6/extend',
+                        'chapter6/overload',
+                        'chapter6/generic'
+                    ]
+                },
+                {
+                    'title': 'ts-axios 拦截器实现',
+                    collapsable: false,
+                    children: [
+                        'chapter7/interceptor'
+                    ]
+                },
+                {
+                    'title': 'ts-axios 配置化实现',
+                    collapsable: false,
+                    children: [
+                        'chapter8/merge',
+                        'chapter8/transform',
+                        'chapter8/create'
+                    ]
+                },
+                {
+                    'title': 'ts-axios 取消功能实现',
+                    collapsable: false,
+                    children: [
+                        'chapter9/cancel'
+                    ]
+                },
+                {
+                    'title': 'ts-axios 更多功能实现',
+                    collapsable: false,
+                    children: [
+                        'chapter10/withCredentials',
+                        'chapter10/xsrf',
+                        'chapter10/upload-download',
+                        'chapter10/auth',
+                        'chapter10/validateStatus',
+                        'chapter10/paramsSerializer',
+                        'chapter10/baseURL',
+                        'chapter10/static'
+                    ]
+                },
+                {
+                    'title': 'ts-axios 单元测试',
+                    collapsable: false,
+                    children: [
+                        'chapter11/preface',
+                        'chapter11/jest',
+                        'chapter11/helpers',
+                        'chapter11/requests',
+                        'chapter11/headers',
+                        'chapter11/instance',
+                        'chapter11/interceptor',
+                        'chapter11/mergeConfig',
+                        'chapter11/cancel',
+                        'chapter11/more'
+                    ]
+                },
+                {
+                    'title': 'ts-axios 部署与发布',
+                    collapsable: false,
+                    children: [
+                        'chapter12/build-deploy',
+                        'chapter12/demo'
+                    ]
+                },
+                {
+                    'title': '课程总结',
+                    collapsable: false,
+                    children: [
+                        'chapter13/summary'
+                    ]
                 }
-              ]
-              break;
+            ]
+            break;
         case 'todo':
             return [
                 ['', '欢迎'],
